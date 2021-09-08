bac75455d6d0b250a3988e093f85e62de1cea0dc
Spencer <Spencer.Taber@yahoo.com>
2021-09-08 17:04:25 +0100

    reducer index.js combine reducers file written

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+/* To make the src index.js file more lean I have created a new index.js 
+in the reducers file to consolidate reducer files and import in main src index.js*/
+import counterReducer from "./counter";
+import loggedReducer from "./counter";
+import { combineReducers } from "redux";
+
+const allReducers = combineReducers({
+    counter: counterReducer,
+    isLogged: loggedReducer
+});
