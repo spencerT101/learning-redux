53e1f33cf35b2b261ecb4e5d313df450cbea997c
Spencer <Spencer.Taber@yahoo.com>
2021-09-08 18:06:26 +0100

    counter, reducer and islogged displaying in browser

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import React from 'react';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment} from "./actions";
+import {decrement} from "./actions";
+
+
 
 function App() {
+  const counter = useSelector (state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
+
   return (
     <div className="App">
-   <h1>Counter</h1>
+   <h1>Counter {counter}</h1>
+   <button onClick = {() => dispatch(increment())}>+</button>
+   <button onClick = {() => dispatch(decrement())}>-</button>
+
+    {isLogged?
+   <h3>Valuable information I shouldn't see if I am not logged in</h3> : ''}
     </div>
   );
 }
