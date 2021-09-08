9039a07364c473abf00b4c4c8c8c8aaa1768b462
Spencer <Spencer.Taber@yahoo.com>
2021-09-08 16:40:41 +0100

    react branch and src files created.

--- src/index.js
+++ src/index.js
@@ -3,48 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux';
-
-//let store = createStore(reducer);
-
-//STORE --> Globalized state
 
 
-
-//ACTION  Increment
-const increment = () =>{
-  return {
-  type: 'INCREMENT'
-  }
-}
-
-  const decrement = () => {
-    return{
-      type: "DECREMENT"
-
-  }
-}
-
-//REDUCER 
-const counter =(state = 0, action) => {
-  switch(action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case 'DECREMENT':
-      return state -1;
-  }
-};
-
-let store = createStore(counter);
-
-//Display in console
-store.subscribe(() => console.log(store.getState()));
-
-//DISPATCH
-
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
