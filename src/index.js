dc2971d45a78fdadb9d74ae7c373cbfd53df3c37
Spencer <Spencer.Taber@yahoo.com>
2021-09-08 15:49:19 +0100

    reducer increment and decrement function written

--- src/index.js
+++ src/index.js
@@ -18,7 +18,22 @@ const increment = () =>{
   }
 }
 
+  const decrement = () => {
+    return{
+      type: "DECREMENT"
+
+  }
+}
+
 //REDUCER 
+const counter =(state = 0, action) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case 'DECREMENT':
+      return state -1;
+  }
+};
 
 //DISPATCH
 
